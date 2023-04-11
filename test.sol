d265e753a30b2b5e237350d2e721d829055e674e
Липатов Максим <103856799+greydatlenght@users.noreply.github.com>
2023-04-11 11:29:55 +0300

    Create test.sol

---
+++ test.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity > 0.7.0;
+
+/**
+ * @title Test
+ * @dev Test Contract
+ */
+contract EstatesContract {
+    address admin = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+    struct Estates {
+        uint id;
+        address owner;
+        uint square;
+        uint yearsOfExpluatation;
+    }
+
+    struct Salling {
+        uint id;
+        bool onSale;
+        uint onSaleTime;
+        uint price;
+
+        bool transfered;
+    }
+
+    Estates[] public houseArrays;
+    Salling[] public sallings;
+
+    constructor() {
+        houseArrays.push(Estates(0, 0xdD870fA1b7C4700F2BD7f44238821C26f7392148, 50, 7));
+        sallings.push(Salling(0, false, 0, 0, false));
+    }
+
+    function newHouseRegister(uint _id, address _owner, uint _square, uint _years) public {
+        require(msg.sender == admin);
+        houseArrays.push(Estates(_id, _owner, _square, _years));
+        sallings.push(Salling(0, false, 0, 0, false));
+    } 
+
+    function goOnSale(uint _Id, uint _SecondsCount, uint _Price) public {
+        require(houseArrays[_Id].owner == msg.sender);
+        require(sallings[_Id].onSale == true);
+
+
+        sallings[_Id].onSale = true;
+        sallings[_Id].onSaleTime = _SecondsCount;
+        sallings[_Id].price = _Price;
+    }
+
+    function cancelSale(uint _Id) public  {
+        require(houseArrays[_Id].owner == msg.sender);
+        require(sallings[_Id].onSale == false);
+
+        sallings[_Id].onSale = false;
+    }
+
+
+    function buyHouse(uint _houseId) public payable {
+        require(msg.sender != houseArrays[_houseId].owner);
+        require(sallings[_houseId].onSale == true);
+        require(msg.value == sallings[_houseId].price);
+        
+        sallings[_houseId].transfered = true;
+
+        changeOwnership(_houseId, msg.sender);
+    }
+
+    // исправить так, чтобы вызывалась продавцом
+    function changeOwnership(uint _houseId, address _owner) internal {
+        require(sallings[_houseId].transfered == true);
+
+        payable(houseArrays[_houseId].owner).transfer(sallings[_houseId].price);
+
+        houseArrays[_houseId].owner = _owner;
+    }
+
+
+    function checkAll() public view returns (Estates[] memory) {
+        return houseArrays;
+    }
+}
